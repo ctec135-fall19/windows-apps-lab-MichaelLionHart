9b654006f7cc95430c732b5612bd2c07051ae3ea
Michael Hart <M.HART@students.clark.edu>
2019-11-14 14:53:55 -0800

    Thursday 11/14 demo

---
+++ AppByHand/SimplestWFApp.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows.Forms;
+
+namespace SimplestWFApp
+{
+	class Program
+	{
+		static void Main() 
+		{
+			Application.Run(new MainWindow());
+		}
+	}
+	
+	class MainWindow : Form 
+	{
+		public MainWindow() { }
+	}
+}
